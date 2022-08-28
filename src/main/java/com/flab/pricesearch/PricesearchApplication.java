26aaad3f800c15e4b8df80093cd46f3d913c6b78
f-lab-scott <hwijun.jeong@team-samaria.com>
2022-08-28 16:17:34 +0900

    initial commit

---
+++ src/main/java/com/flab/pricesearch/PricesearchApplication.java
@@ -0,0 +1,13 @@
+package com.flab.pricesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricesearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PricesearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flab/pricesearch/PricesearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.pricesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
