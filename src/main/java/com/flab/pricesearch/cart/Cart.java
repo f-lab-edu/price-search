727f8baa0494b698784754948ce4bfc9019dfadc
Gangbean <xbox2537@gamil.com>
2022-10-05 21:28:02 +0900

    checkstyle 적용

--- src/main/java/com/flab/pricesearch/cart/Cart.java
+++ src/main/java/com/flab/pricesearch/cart/Cart.java
@@ -2,9 +2,14 @@ package com.flab.pricesearch.cart;
 
 import com.flab.pricesearch.product.Product;
 import com.flab.pricesearch.user.domain.User;
-
-import javax.persistence.*;
 import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.JoinTable;
 
 @Entity
 public class Cart {
