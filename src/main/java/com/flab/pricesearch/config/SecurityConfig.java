7de6735c7713f4499be40e88c6f5d14d66ab3d74
kgi <rudrbdlf1123@gmail.com>
2022-10-16 13:13:44 +0900

    SpringSecurity 변경

--- src/main/java/com/flab/pricesearch/config/SecurityConfig.java
+++ src/main/java/com/flab/pricesearch/config/SecurityConfig.java
@@ -1,24 +1,52 @@
 package com.flab.pricesearch.config;
 
+import com.flab.pricesearch.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    UserService userService;
     @Override
-    protected void configure(HttpSecurity http) {
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+            .loginPage("/users/login")
+            .defaultSuccessUrl("/")
+            .usernameParameter("userId")
+            .failureUrl("/users/login/error")
+            .and()
+            .logout()
+            .logoutRequestMatcher(new AntPathRequestMatcher("/users/logout"))
+            .logoutSuccessUrl("/");
+
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+            .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
     }
 
 }
