56c6cdda4e70310b2ad2e8a287c3b2f70b12c7fc
kgi <rudrbdlf1123@gmail.com>
2022-10-16 13:14:56 +0900

    UserService 추가

--- src/main/java/com/flab/pricesearch/user/service/UserService.java
+++ src/main/java/com/flab/pricesearch/user/service/UserService.java
@@ -1,15 +1,21 @@
 package com.flab.pricesearch.user.service;
 
+
+import com.flab.pricesearch.user.domain.SpringSecurityUser;
 import com.flab.pricesearch.user.domain.User;
 import com.flab.pricesearch.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
@@ -23,6 +29,21 @@ public class UserService {
         if (findUser != null) {
             throw new IllegalStateException("이미 가입된 회원 입니다.");
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        User user = userRepository.findByUserId(userId);
+
+        if (user == null) {
+            throw new UsernameNotFoundException(userId);
+        }
+
+        return SpringSecurityUser.builder()
+            .username(user.getUserId())
+            .password(user.getPassword())
+            .roles(user.getRoleType().toString())
+            .build();
     }
 }
 
