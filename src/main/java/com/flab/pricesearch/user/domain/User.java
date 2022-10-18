33db94ccc78c3cba84e398b627d5f56aa81542bd
Gangbean <xbox2537@gamil.com>
2022-10-19 00:56:10 +0900

    :construction: rebase 전 commit

--- src/main/java/com/flab/pricesearch/user/domain/User.java
+++ src/main/java/com/flab/pricesearch/user/domain/User.java
@@ -1,54 +1,25 @@
 package com.flab.pricesearch.user.domain;
 
-import com.flab.pricesearch.cart.Cart;
-import java.util.List;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import lombok.Data;
 
 @Data
-@Entity
 public class User {
-
-    @Id
-    private String userId;
-
-    private String password;
-
-    private String platformType;
-
-    private String token;
-
-    private String userName;
-
-    private String birthDate;
-
-    private String gender;
-
-    private String tel;
-
-    private String email;
-
-    private String address;
-
-    private String regDate;
-
-    private String termsAgreeYn;
-
-    private String termsAgreeDateTime;
-
-    private String dormantYn;
-
-    private String dormantDate;
-
-    private String withdrawalDate;
-
-    private String status;
-
-    private String lastUpdateDate;
-
-    @OneToMany(mappedBy = "user")
-    private List<Cart> cartList;
-
+  private String userId;
+  private String password;
+  private String platformType;
+  private String token;
+  private String userName;
+  private String birthDate;
+  private String gender;
+  private String tel;
+  private String email;
+  private String address;
+  private String regDate;
+  private String termsAgreeYn;
+  private String termsAgreeDateTime;
+  private String dormantYn;
+  private String dormantDate;
+  private String withdrawalDate;
+  private String status;
+  private String lastUpdateDate;
 }
